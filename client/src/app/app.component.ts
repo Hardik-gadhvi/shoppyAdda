61f79486824ef0578af40e1a32c6ebdb9525db06
Hardik-gadhvi <gadhvi.hardik1159@gmail.com>
2024-08-04 20:01:32 +0530

    built shop functionality with integrating, search, pagination functionality on UI end.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { Product } from './shared/Models/porduct';
+import { Pagination } from './shared/Models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+
+  constructor(){}
+
 }
